8541851ef50ad18788c2f63169ba061b68ce8a6f
jack <jack@is-land.com.tw>
2016-03-31 16:38:02 +0800

    add log file

--- src/main/java/org/apache/hbase/coprocessor/CoprocessorManager.java
+++ src/main/java/org/apache/hbase/coprocessor/CoprocessorManager.java
@@ -2,6 +2,8 @@ package org.apache.hbase.coprocessor;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.Coprocessor;
 import org.apache.hadoop.hbase.HTableDescriptor;
@@ -10,6 +12,8 @@ import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class CoprocessorManager {
+	private static final Log LOG = LogFactory.getLog(CoprocessorManager.class.getName());
+
 	private HBaseAdmin admin;
 	
 	public CoprocessorManager(HBaseAdmin admin){
@@ -22,6 +26,7 @@ public class CoprocessorManager {
 			for(TableName tableName : tableNames){
 				this.dropCoprocessor(tableName.getNameAsString());
 			}
+			LOG.info("DROP ALL TABLE COPERCOESSOR FINISH");
 		}catch(Exception e){
 			throw new RuntimeException(e);
 		}
@@ -29,6 +34,7 @@ public class CoprocessorManager {
 	
 	public void dropCoprocessor(String tableName){
 		try{
+			LOG.info("drop " + tableName + " table coprocessor");
 			HTableDescriptor htableDescriptor = this.admin.getTableDescriptor(Bytes.toBytes(tableName));
 			List<String> coprocessors = htableDescriptor.getCoprocessors();
 			for(String coprocessor : coprocessors){
@@ -46,7 +52,7 @@ public class CoprocessorManager {
 			for(TableName tableName : tableNames){
 				addCoprocessor(tableName.getNameAsString(), hdfsJarFilePath, className);
 			}
-			
+			LOG.info("ADD ALL TABLE COPROCESSOR FINISH");
 		}catch(Exception e){
 			throw new RuntimeException(e);
 		}
@@ -54,6 +60,7 @@ public class CoprocessorManager {
 	
 	public void addCoprocessor(String tableName, String hdfsJarFilePath, String className){
 		try{
+			LOG.info("Add " + tableName + " table coprocessor");
 			HTableDescriptor htableDescriptor = this.admin.getTableDescriptor(Bytes.toBytes(tableName));
 			htableDescriptor.addCoprocessor(className, new Path(hdfsJarFilePath), Coprocessor.PRIORITY_USER, null);
 			this.admin.modifyTable(tableName, htableDescriptor);
