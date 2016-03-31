6ffc177e7bad71876824330e42aadaa046d740dd
jack <jack@is-land.com.tw>
2016-03-31 16:12:44 +0800

    add shell script example

---
+++ shell/dropAllTableCoprocessor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export zookeeperHost=server-a1
+
+java -cp $HBASE_HOME/lib/*:hbase-api-example.jar:. org.apache.hbase.coprocessor.main.DropAllTableCoprocessorMain
---
+++ shell/registerOneTableCoprocessor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export zookeeperHost=server-a1
+
+java -cp $HBASE_HOME/lib/*:hbase-api-example.jar:. org.apache.hbase.coprocessor.main.RegisterOneTableCoprocessorMain table100 hdfs://server-a1:9000/tmp/haredb35.jar com.haredb.hbase.hive.coprocessor.BaseHiveQLEndpoint
